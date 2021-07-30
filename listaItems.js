e31d6052330f34b797f65fe3c3410c1fd0954620
p3dr0m4th3us <46729924+p3dr0m4th3us@users.noreply.github.com>
2021-07-30 18:12:42 -0300

    Add files via upload

---
+++ listaItems.js
@@ -0,0 +1,18 @@
+const listaItems = [
+  {
+    id: '01',
+    src: './img/1.png'
+  },
+  {
+    id: '02',
+    src: './img/2.png'
+  },
+  {
+    id: '03',
+    src: './img/3.png'
+  },
+  {
+    id: '04',
+    src: './img/4.png'
+  },
+];
